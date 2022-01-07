f7e74029a061ff8ee0334182069b84c3d2f93c96
Louise <bretzlouise@gmail.com>
2022-01-07 10:28:41 +0100

    Structure de base + BLL + Presentation

---
+++ C#/BLL2/ImageManager/ImageFilters.cs
@@ -0,0 +1,84 @@
+﻿using System.Drawing;
+namespace BLL
+{
+    public class ImageFilters : IImageFilters
+    {
+        public Bitmap BlackWhite(Bitmap initial)
+        {
+            Bitmap Bmp = new Bitmap(initial);
+            int rgb;
+            Color c;
+
+            for (int y = 0; y < Bmp.Height; y++)
+                for (int x = 0; x < Bmp.Width; x++)
+                {
+                    c = Bmp.GetPixel(x, y);
+                    rgb = (int)((c.R + c.G + c.B) / 3);
+                    Bmp.SetPixel(x, y, Color.FromArgb(rgb, rgb, rgb));
+                }
+            return Bmp;
+        }
+
+        //public Bitmap RainbowFilter(Bitmap bmp, BackgroundWorker worker)
+        public Bitmap RainbowFilter(Bitmap bmp)
+        {
+
+            Bitmap temp = new Bitmap(bmp.Width, bmp.Height);
+            int raz = bmp.Height / 4;
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int x = 0; x < bmp.Height; x++)
+                {
+
+                    if (i < (raz))
+                    {
+                        temp.SetPixel(i, x, Color.FromArgb(bmp.GetPixel(i, x).R / 5, bmp.GetPixel(i, x).G, bmp.GetPixel(i, x).B));
+                    }
+                    else if (i < (raz * 2))
+                    {
+                        temp.SetPixel(i, x, Color.FromArgb(bmp.GetPixel(i, x).R, bmp.GetPixel(i, x).G / 5, bmp.GetPixel(i, x).B));
+                    }
+                    else if (i < (raz * 3))
+                    {
+                        temp.SetPixel(i, x, Color.FromArgb(bmp.GetPixel(i, x).R, bmp.GetPixel(i, x).G, bmp.GetPixel(i, x).B / 5));
+                    }
+                    else if (i < (raz * 4))
+                    {
+                        temp.SetPixel(i, x, Color.FromArgb(bmp.GetPixel(i, x).R / 5, bmp.GetPixel(i, x).G, bmp.GetPixel(i, x).B / 5));
+                    }
+                    else
+                    {
+                        temp.SetPixel(i, x, Color.FromArgb(bmp.GetPixel(i, x).R / 5, bmp.GetPixel(i, x).G / 5, bmp.GetPixel(i, x).B / 5));
+                    }
+                }
+
+            }
+            return temp;
+        }
+
+
+
+        //apply color filter to swap pixel colors
+        public Bitmap SwapFilter(Bitmap bmp)
+        {
+
+            Bitmap temp = new Bitmap(bmp.Width, bmp.Height);
+
+
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int x = 0; x < bmp.Height; x++)
+                {
+                    Color c = bmp.GetPixel(i, x);
+                    Color cLayer = Color.FromArgb(c.A, c.G, c.B, c.R);
+                    temp.SetPixel(i, x, cLayer);
+                }
+
+            }
+            return temp;
+        }
+
+
+
+    }
+}
---
+++ C#/BLL2/ToolsManager/ICanvasManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Drawing;
+
+namespace BLL
+{
+    public interface ICanvasManager
+    {
+        Bitmap copyToSquareCanvas(Bitmap sourceBitmap, int canvasWidthLenght);
+    }
+}
